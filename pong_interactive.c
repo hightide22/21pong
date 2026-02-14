9d56ef4e248e9ab18e5628003aa1191da8a01a7a
hightide22 <109390197+hightide22@users.noreply.github.com>
2026-02-14 16:04:21 +0300

    Update pong_interactive.c

--- pong_interactive.c
+++ pong_interactive.c
@@ -1,12 +1,8 @@
-#include <stdio.h>
 #include <ncurses.h>
-
-int draw(int l_rocket, int r_rocket, int x_ball, int y_ball, int l_score, int r_score) {
-    return l_rocket + r_rocket + x_ball + y_ball + l_score + r_score; //заглушка
-}
+#include <stdio.h>
 
 int get_y_acc(int y_ball, int x_ball, int y_acc, int x_acc, int l_rocket, int r_rocket) {
-    int r = y_acc; 
+    int r = y_acc;
     if (y_acc == 1 && y_ball == 24) {
         r = -1;
     }
@@ -31,7 +27,7 @@ int get_y_acc(int y_ball, int x_ball, int y_acc, int x_acc, int l_rocket, int r_
     }
     return r;
 };
-int get_x_acc(int y_ball, int x_ball, int x_acc, int l_rocket, int r_rocket){
+int get_x_acc(int y_ball, int x_ball, int x_acc, int l_rocket, int r_rocket) {
     int r = x_acc;
     if (x_ball == 78 && x_acc == 1) {
         if (r_rocket == y_ball || r_rocket == y_ball + 1 || r_rocket == y_ball - 1) {
@@ -48,10 +44,10 @@ int get_x_acc(int y_ball, int x_ball, int x_acc, int l_rocket, int r_rocket){
 int move_rocket(int pos, char c) {
     int r = pos;
     if (c == 'A' || c == 'K') {
-        if (pos != 1){
-            r = pos - 1; 
+        if (pos != 1) {
+            r = pos - 1;
         }
-    } else if (c == 'x' || c == ',') {
+    } else if (c == 'Z' || c == 'M') {
         if (pos != 23) {
             r = pos + 1;
         }
@@ -59,7 +55,7 @@ int move_rocket(int pos, char c) {
     return r;
 }
 
-int get_goal(int x_ball){
+int get_goal(int x_ball) {
     int r = 0;
     if (x_ball == 0) {
         r = 1;
@@ -72,33 +68,59 @@ int get_goal(int x_ball){
 int is_win(int r_s, int l_s) {
     int r = 0;
     if (l_s == 21) {
-        printf("Left won!");
+        printw("Left won!");
         r = 1;
     }
     if (r_s == 21) {
-        printf("Righ won!");
+        printw("Righ won!");
         r = 1;
     }
     return r;
 }
+
+// void clear_screen() { printw("\e[1;1H\e[2J"); }
+
+void draw(int l_rocket, int r_rocket, int x_ball, int y_ball, int l_score, int r_score) {
+    printw("Score: %d : %d\n\n\n", l_score, r_score);
+    for (int i = -1; i <= 25; i++) {
+        for (int j = 0; j < 80; j++) {
+            if ((j == 1 && (i == l_rocket || i == l_rocket - 1 || i == l_rocket + 1)) ||
+                (j == 78 && (i == r_rocket || i == r_rocket - 1 || i == r_rocket + 1))) {
+                printw("|");
+            } else if (i == -1 || i == 25) {
+                printw("_");
+            } else if (y_ball == i && x_ball == j) {
+                printw("*");
+            } else {
+                printw(" ");
+            }
+        }
+        printw("\n");
+    }
+}
+
 void init() {
     initscr();
-    cbreak();               // Отключаем буферизацию строк
-    noecho();               // Не отображаем вводимые символы
-    keypad(stdscr, TRUE);   // Включаем специальные клавиши
-    nodelay(stdscr, TRUE);  // Неблокирующий ввод
-    curs_set(0);            // Скрываем курсор
+    cbreak();              // Отключаем буферизацию строк
+    noecho();              // Не отображаем вводимые символы
+    keypad(stdscr, TRUE);  // Включаем специальные клавиши
+    // nodelay(stdscr, TRUE);          // Неблокирующий ввод
+    halfdelay(1);
+    curs_set(0);           // Скрываем курсор
 }
-void logic(){
-    init();
+void logic() {
     int l_ro = 12, r_ro = 12, x_ball = 40, y_ball = 12, x_acc = -1, y_acc = 0, l_s = 0, r_s = 0, turn = 0;
     char input;
+    printf("Left input:");
+    while (scanf("%c", &input) != 1 || (input != 'A' && input != 'Z' && input != ' '));
+    while (scanf("%c", &input) != 1 || (input != 'K' && input != 'M' && input != ' '));
+    printf("\n");
+    init();
+
     while (1) {
-        clear();
-        input = ' ';
         input = getch();
         if (turn) {
-            if((input == 'A') || (input == 'Z' )) l_ro = move_rocket(l_ro, input);
+            if ((input == 'A') || (input == 'Z')) l_ro = move_rocket(l_ro, input);
         } else {
             if ((input == 'K') || (input == 'M')) r_ro = move_rocket(r_ro, input);
         }
@@ -106,7 +128,7 @@ void logic(){
         x_acc = get_x_acc(y_ball, x_ball, x_acc, l_ro, r_ro);
         x_ball += x_acc;
         y_ball += y_acc;
-        if (get_goal(x_ball) != 0){
+        if (get_goal(x_ball) != 0) {
             if (get_goal(x_ball) == 1) {
                 r_s++;
             } else {
@@ -114,13 +136,16 @@ void logic(){
             }
             l_ro = y_ball = r_ro = 12;
             x_ball = 40;
+            y_acc = 0;
         }
         turn = turn ^ 1;
-        refresh(); 
-        usleep(50000);
+        refresh();
+        // napms(50);
+        clear();
         draw(l_ro, r_ro, x_ball, y_ball, l_s, r_s);
-        if (is_win(r_s, l_s)){
+        if (is_win(r_s, l_s)) {
             break;
+            refresh();
         }
     }
 }
